45c7b14b7c216fa5710703346a5a151a0d5cac8c
Simon Andersson <simon.andersson@monterosa.se>
2013-10-01 13:26:20 +0200

    initial commit

---
+++ md2html/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fredrik
+//
+//  Created by Simon Andersson on 10/1/13.
+//  Copyright (c) 2013 Monterosa AB. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ md2html/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Fredrik
+//
+//  Created by Simon Andersson on 10/1/13.
+//  Copyright (c) 2013 Monterosa AB. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
